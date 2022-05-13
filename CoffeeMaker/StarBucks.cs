f070b13fb77d06694271d6450a7f75f501d8522c
Your Name <you@example.com>
2022-05-13 14:03:15 +0530

    [Megha] Add: Welcome To MSUnit Testing

---
+++ CoffeeMaker/StarBucks.cs
@@ -0,0 +1,41 @@
+﻿namespace CoffeeMaker
+{
+    public class StarBucks : IMakeACoffee
+    {
+        public bool CheckingIngredientsAvailability()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CoffeeMaking(int sugerPerSpoon, int coffeePack)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class StubStarBuks : IMakeACoffee
+    {
+        public bool CheckingIngredientsAvailability()
+        {
+            return true;
+        }
+
+        public string CoffeeMaking(int sugerPerSpoon, int coffeePack)
+        {
+            return "Your Coffee is Recieve";
+        }
+    }
+    public class FakeStarBuks : IMakeACoffee
+    {
+        public bool CheckingIngredientsAvailability()
+        {
+            return true;
+        }
+
+        public string CoffeeMaking(int sugerPerSpoon, int coffeePack)
+        {
+            return "Your Coffee is Recieve";
+        }
+    }
+
+}
+
---
+++ StarBucksMSTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StarBucksMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
